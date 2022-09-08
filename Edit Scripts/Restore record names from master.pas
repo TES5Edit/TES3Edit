7d777a8edfa3c9c212b847b32b4da98044a2834d
Gawrion <82583728+Gawrion@users.noreply.github.com>
2022-09-08 17:46:04 +0200

    Fixed bugs and additional functions
    I fixed the bug when script was searching not for many masters but form many changes to master.
    I also added test mode.
    Also added some obvious exceptions (when master is empty, when master is space string or when name is reserved in Update,esm for cc content). There is a space to add some more exceptions (fe. in configured array).
    There is also added some simple output for analyzing.

--- Edit Scripts/Restore record names from master.pas
+++ Edit Scripts/Restore record names from master.pas
@@ -8,7 +8,10 @@ unit UserScript;
 function Process(e: IInterface): integer;
 var
   m: IInterface;
+  mode: Char;
 begin
+// if U want only test (withoult apllying changes) set variable as 'test':	
+  mode:= 'test';
   if not ElementExists(e, 'FULL') and not ElementExists(e, 'DESC') then
     Exit;
 
@@ -20,13 +23,58 @@ begin
     Exit;
   
   // if record overrides several masters, then get the last one
-  if OverrideCount(m) > 1 then
-    m := OverrideByIndex(m, OverrideCount(m) - 2);
+  if MasterCount(m) > 1 then
+	begin
+	m := OverrideByIndex(m, OverrideCount(m) - 2);
+	AddMessage('---------------------------------------------------------------------------------------');
+	AddMessage('more than one master. Setting master to: '+FullPath(m)+);
+	end;
 
   if not SameText(GetElementEditValues(e, 'FULL'), GetElementEditValues(m, 'FULL')) then
-    SetElementEditValues(e, 'FULL', GetElementEditValues(m, 'FULL'));
+	begin
+	AddMessage('---------------------------------------------------------------------------------------');
+	AddMessage('element: '+GetElementEditValues(e, 'FULL')+' ('+ShortName(e)+')');
+	AddMessage('master: '+GetElementEditValues(m, 'FULL')+' ('+ShortName(m)+')');
+	if (GetElementEditValues(m, 'FULL') = '') or (GetElementEditValues(m, 'FULL') = ' ') or (GetElementEditValues(m, 'FULL') = 'Creation Club Cell') then
+		begin
+			if not (mode = 'test') then
+				begin
+				AddMessage('master string EMPTY or master is ''Creation Club Cell'' - not restoring');
+				end else 
+				AddMessage('TEST MODE - no changes to plugins');
+
+		Exit;
+		end else
+			begin
+			if not (mode = 'test') then
+				begin
+				SetElementEditValues(e, 'FULL', GetElementEditValues(m, 'FULL')); 
+	   			AddMessage('restoring FULL string: '+GetElementEditValues(m, 'FULL'));
+				end else 
+				AddMessage('TEST MODE - no changes to plugins');	
+			end;
+		end;
+
+
   if not SameText(GetElementEditValues(e, 'DESC'), GetElementEditValues(m, 'DESC')) then
-    SetElementEditValues(e, 'DESC', GetElementEditValues(m, 'DESC'));
+    	begin
+	AddMessage('---------------------------------------------------------------------------------------');
+	AddMessage('element: '+GetElementEditValues(e, 'DESC')+' ('+ShortName(e)+')');
+	AddMessage('master: '+GetElementEditValues(m, 'DESC')+' ('+ShortName(m)+')');
+	if (GetElementEditValues(m, 'DESC') = '') or (GetElementEditValues(m, 'DESC') = ' ') or (GetElementEditValues(m, 'DESC') = 'Creation Club Cell') then
+		begin
+		AddMessage('master string EMPTY or master is ''Creation Club Cell'' - not restoring');
+		Exit;
+		end else
+			begin
+			if not (mode = 'test') then
+				begin
+				SetElementEditValues(e, 'DESC', GetElementEditValues(m, 'DESC')); 
+   				AddMessage('restoring DESC string: '+GetElementEditValues(m, 'DESC'));
+				end else 
+				AddMessage('TEST MODE - no changes to plugins');
+			end;
+		end;
 end;
 
 end.
